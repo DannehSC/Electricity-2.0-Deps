56382f6a999b1a6ccdb230e1d9cbf010bcb60bb3
DannehSC <dangerspookycanyon@gmail.com>
2017-11-21 16:04:15 -0600

    .

--- luvit-reql/init.lua
+++ luvit-reql/init.lua
@@ -1,15 +1,16 @@
 local connect = require('connect')
 local reql = require('Utils/reql.lua')
 local logger = require('Utils/logger.lua')
+local emitter = require('Utils/emitty.lua')
 
 -- Default options table || Do NOT edit, pass your options to the function instead
 
 local default = {
-    address = 'http://127.0.0.1',
-    port = 28015,
-    user = 'admin',
-    password = '',
-    db = 'test',
+	address = 'http://127.0.0.1',
+	port = 28015,
+	user = 'admin',
+	password = '',
+	db = 'test',
 	reconnect = false,
 	reusable = false,
 }
@@ -19,11 +20,11 @@ local sub, len, find = string.sub, string.len, string.find
 
 return {
 	connect = function(options)
-        options = options and options or {}
-        local type = type(options)
-        if not type == 'table' then
-            error(format('Bad argument #1 to luvit-reql/init.lua, table expected, got %s', type))
-        end
+		options = options and options or {}
+		local type = type(options)
+		if type ~= 'table' then
+			return error(format('Bad argument #1 to luvit-reql.connect(), table expected, got %s', type))
+		end
 		for k,v in pairs(default) do
 			if not options[k] then
 				options[k] = v
@@ -32,8 +33,8 @@ return {
 		if options.address:sub(#options.address)=='/'then
 			options.address = options.address:sub(1,#options.address-1)
 		end
-        if not find(options.address, 'https?', 1) == 1 then
-            logger.warn.format('Procotol not supplied, defaulting to http://')
+		if not find(options.address, 'https?', 1) == 1 then
+			logger.warn('Procotol not supplied, defaulting to http://')
 			options.address = format('http://%s', options.address)
 		end
 		return connect(options)
@@ -41,5 +42,9 @@ return {
 
 	reql = function()
 		return reql()
-	end
+	end,
+	
+	emitter = emitter,
+	
+	logger = logger
 }
--- luvit-reql/libs/connect.lua
+++ luvit-reql/libs/connect.lua
@@ -9,9 +9,12 @@ local logger=require('./Utils/logger.lua')
 local compare_digest=require('./Utils/compare.lua')
 local cmanager=require('./Utils/coroutinemanager.lua')
 local process=require('./Utils/processor.lua').processData
-local xor,bxor256=x[1],x[2]
-local concat,gmatch,format=table.concat,string.gmatch,string.format
-local checkCoroutine=cmanager.isCoro
+
+local xor, bxor256 = x[1], x[2] -- NOTE: unused variable
+
+local concat, gmatch, format = table.concat, string.gmatch, string.format
+local checkCoroutine = cmanager.isCoro
+
 local function new_token()
 	local var = 0
 	local function get_token()
@@ -21,46 +24,46 @@ local function new_token()
 	return get_token
 end
 local function copy(t)
-	local n={}
-	for k,v in pairs(t)do
-		n[k]=v
+	local n = {}
+	for k, v in pairs(t)do
+		n[k] = v
 	end
 	return n
 end
 local connect
 function connect(options)
-	local socket={
-		closed=false
+	local socket = {
+		closed = false
 	}
-	local addr=options.address
-	addr=addr:gsub('https://','')
-	addr=addr:gsub('http://','')
-	local tls=options.address:sub(1,5)=='https'
+	local addr = options.address
+	addr = addr:gsub('https://','')
+	addr = addr:gsub('http://','')
+	local tls = options.address:sub(1, 5) == 'https' -- NOTE: unused variable
 	local function connectToRethinkdb()
-		local opt=copy(options)
-		local stuff={net.connect({
-			host=addr,
-			port=options.port,
+		local opt = copy(options)
+		local stuff = {net.connect({
+			host = addr,
+			port = options.port,
 		})}
-		logger.info.format(format('Connecting to %s:%s',addr,options.port))
-		local read,write,close=stuff[1],stuff[2],stuff[6]
-		if type(write)=="string"then
-			socket.closed=true
-			return logger.err.format("Socket",write)
+		logger.info(format('Connecting to %s:%s', addr, options.port))
+		local read, write, close = stuff[1], stuff[2], stuff[6]
+		if type(write) == "string"then
+			socket.closed = true
+			return logger.err("Socket", write)
 		end
-		socket.read=read
-		socket.write=write
-		socket.close=function()
-			socket.closed=true
+		socket.read = read
+		socket.write = write
+		socket.close = function()
+			socket.closed = true
 			close()
 		end
-		local user,auth_key=options.user,options.password
+		local user, auth_key = options.user, options.password
 		-- Initiation (First Client Message/First Server Challenge)
 		write(string.pack('<I', 0x34c2bdc3))
-		local success,res = pcall(function() return json.decode(read()) end)
+		local success, res = pcall(function() return json.decode(read()) end) -- NOTE: unused variable
 		if not success then
 			socket.close()
-			return logger.err.format(errors.ReqlDriverError('Error reading JSON data.'))
+			return logger.err(errors.ReqlDriverError('Error reading JSON data.'))
 		end
 		local nonce = ssl.base64(ssl.random(18), true)
 		local client_first_message = 'n=' .. user .. ',r=' .. nonce
@@ -74,7 +77,7 @@ function connect(options)
 		res = json.decode(read())
 		if not res.success then
 			socket.close()
-			return logger.err.format(errors.ReqlAuthError("Error: "..res.error))
+			return logger.err(errors.ReqlAuthError("Error: "..res.error))
 		end
 		local auth = {}
 		local server_first_message = res.authentication
@@ -86,10 +89,10 @@ function connect(options)
 		auth.i = tonumber(auth.i)
 		local client_final_message = 'c=biws,r=' .. auth.r
 		local salt = ssl.base64(auth.s, false)
-		local salted_password, salt_error = pbkdf('sha256', auth_key, salt, auth.i, 32)
+		local salted_password, salt_error = pbkdf('sha256', auth_key, salt, auth.i, 32) -- NOTE: unused variable
 		if not salted_password then
 			socket.close()
-			return logger.err.format(errors.ReqlDriverError("Salt error"))
+			return logger.err(errors.ReqlDriverError("Salt error"))
 		end
 		local ckHMAC = ssl.hmac.new('sha256',salted_password)
 		local client_key = ckHMAC:final('Client Key', true)
@@ -109,13 +112,13 @@ function connect(options)
 			if options.debug then
 				p('DEBUG',res.error)
 			end
-			return logger.err.format(errors.ReqlAuthError("Error: "..res.error))
+			return logger.err(errors.ReqlAuthError("Error: "..res.error))
 		end
 		for k,v in gmatch(res.authentication..',','([vV])=(.-),')do
 			auth[k] = v
 		end
 		if not auth.v then
-			return logger.err.format(errors.ReqlDriverError("Missing server signature"))
+			return logger.err(errors.ReqlDriverError("Missing server signature"))
 		end
 		local skHMAC = ssl.hmac.new('sha256', salted_password)
 		local server_key = skHMAC:final('Server Key', true)
@@ -123,18 +126,15 @@ function connect(options)
 		local server_signature = ssHMAC:final(auth_message, true)
 		if not compare_digest(auth.v, server_signature)then
 			socket.close()
-			return logger.err.format(errors.ReqlAuthError("Invalid server signature"))
+			return logger.err(errors.ReqlAuthError("Invalid server signature"))
 		end
-		logger.info.format(format("Connection to %s:%s complete.",addr,options.port))
+		logger.info(format("Connection to %s:%s complete.",addr,options.port))
 		coroutine.wrap(function()
 			for data in read do
-				if options.debug then
-					p(data)
-				end
 				process(data)
 			end
 			socket.closed=true
-			logger.warn.format(format('Connection to %s:%s closed.',addr,options.port))
+			logger.warn(format('Connection to %s:%s closed.',addr,options.port))
 			if options.reconnect then
 				connect(opt)
 			end
@@ -143,20 +143,21 @@ function connect(options)
 	if checkCoroutine()then
 		connectToRethinkdb()
 	else
-		coroutine.wrap(connectToRethinkdb)()
+		logger.hErr('Cannot connect outside of coroutine currently. Sorry.')
+		--coroutine.wrap(connectToRethinkdb)()
 	end
-	options.password='<HIDDEN>'
-	local conn=setmetatable({
-		_socket=socket,
-		_getToken=new_token(),
-		_options=options,
-		close=function()
-			logger.info.format('Closing socket, cleaning up.')
-			options.reconnect=false
+	options.password = '<HIDDEN>'
+	local conn = setmetatable({
+		_socket = socket,
+		_getToken = new_token(),
+		_options = options,
+		close = function()
+			logger.info('Closing socket, cleaning up.')
+			options.reconnect = false
 			socket.close()
 		end
 	},{})
-	conn.reql=function()
+	conn.reql = function()
 		return reql(conn)
 	end
 	return conn
